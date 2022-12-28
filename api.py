61a1e81fbc8b376f597ab64cf9c52c14c0c6c547
NDX <kraynov.kirill2015@yandex.ru>
2022-12-28 10:54:22 +0300

    update

--- api.py
+++ api.py
@@ -11,8 +11,11 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(dir_path, 'credients.json')
 
 class BigQueryExecuter(Resource):
-    def get(self, start_ind):
+    def get(self, start_ind, end_ind = -1):
         client = bigquery.Client()
+
+        if end_ind == -1:
+            end_ind = start_ind + 100
 
         query = """
             select *  from `discount_db.discount_item`
@@ -34,7 +37,7 @@ class BigQueryExecuter(Resource):
             'load_date': row['load_date']
         } for row in results ]
 
-        return list_of_item[start_ind:]
+        return list_of_item[start_ind:end_ind:end_ind]
 
 
 class BigQueryExecuterWithParamOfResource(Resource):
@@ -64,8 +67,35 @@ class BigQueryExecuterWithParamOfResource(Resource):
         return list_of_item[start_ind:]
 
 
-api.add_resource(BigQueryExecuter, '/<int:start_ind>')
-api.add_resource(BigQueryExecuterWithParamOfResource, '/<string:name_of_site>/<int:start_ind>')
+class BigQueryExecuterWithRegex(Resource):
+    def get(self, start_ind, end_ind, regex):
+        client = bigquery.Client()
+
+        query = """
+            select *  from `discount_db.discount_item`
+            where load_date = '{1}' and ( lower(name) like lower('%{2}%') or lower(category) like lower('%{3}%') )
+            limit {0}
+        """.format(start_ind + 100, datetime.date.today().__str__(), regex, regex)
+        results = client.query(query)
+        
+        list_of_item = [{
+            'id': row['id'],
+            'name_of_site': row['name_of_site'],
+            'category': row['category'],
+            'subcategory': row['subcategory'],
+            'name': row['name'],
+            'details': row['details'],
+            'price': row['price'],
+            'discount': row['discount'],
+            'url': row['url'],
+            'load_date': row['load_date']
+        } for row in results ]
+
+        return list_of_item[start_ind:end_ind]
+
+
+api.add_resource(BigQueryExecuter, '/<int:start_ind>/<int:end_ind>')
+api.add_resource(BigQueryExecuterWithRegex, '/<string:regex>/<int:start_ind>/<int:end_ind>')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
